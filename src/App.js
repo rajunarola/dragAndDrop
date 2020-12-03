946d94b211451473b8d505a5078960f88cc4ec95
Rajvi Khatri <rak@narola.email>
2020-12-03 09:45:40 +0530

    Design Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
+import Dashboard from './Components/Dashboard';
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/home" />
+            </Route>
+            <Route path="/home" exact component={Dashboard} />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          </Switch>
+
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
